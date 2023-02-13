352d7573b626177ebfbd94168b7f643ea8f7d6e5
Paras Nagpal <parasnagpal0707@192.168.1.10>
2023-02-13 10:50:04 +0530

    layout completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
-import { Layout, Typography , Space } from 'antd'
+import { Layout, Typography , Space, Switch } from 'antd'
+import {Link} from 'react-router-dom'
+import {Navbar,Exchanges,Homepage,CryptoDetails,News,Cryptocurrencies} from './components'
+import './App.css'
 
 const App = () => {
   return (
     <div className='app'>
         <div className='navbar'>
-
+               <Navbar/>
         </div>
         <div className='main'>
-
+             <Layout>
+                <div className='routes'>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Homepage/>
+                        </Route>
+                        <Route exact path='/exchanges'>
+                            <Exchanges/>
+                        </Route>
+                        <Route exact path='/cryptocurrencies'>
+                            <Cryptocurrencies/>
+                        </Route>
+                        <Route exact path='/crypto/:coinId'>
+                            <CryptoDetails/>
+                        </Route>
+                        <Route exact path='/news'>
+                            <News/>
+                        </Route>
+                    </Switch>
+                </div>
+             </Layout>
+             <div className='footer'>
+             <Typography.Title level={5} style={{color : 'white'}}>
+                    Cryptoverse <br/>
+                    All rights reserved 
+             </Typography.Title>
+             <Space>
+                <Link to='/'>Home</Link>
+                <Link to='/exchanges'>Exchanges</Link>
+                <Link to='/news'>News</Link>
+             </Space>
         </div>
-        <div className='footer'>
-
         </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
+//import "antd/dist/antd.css"
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+
+
+ReactDOM.render(
+<Router>
+<App/>
+</Router>
+,document.getElementById('root'));
