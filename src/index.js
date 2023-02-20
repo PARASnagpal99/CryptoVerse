a771fd3fd16515cf2775ff0e4388707979b1de5b
Paras Nagpal <parasnagpal0707@Parass-MacBook-Air.local>
2023-02-21 02:45:16 +0530

    Added CryptoCoins Page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
-import store from './app/store'
+import store from './app/store.js'
 //import './antd/dist/antd.css';
 import 'antd/dist/reset.css'
 import { Provider } from "react-redux";
@@ -11,7 +11,7 @@ import { Provider } from "react-redux";
 ReactDOM.render(
 <Router>
     <Provider store={store}>
-<App/>
+         <App/>
     </Provider>
 </Router>
 ,document.getElementById('root'));
